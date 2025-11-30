6d6bb84ef14bf13de9a264019cb31cbd4d484686
noamdj02-netizen <noamdj02-netizen@users.noreply.github.com>
2025-11-30 18:00:15 +0100

    Switch to simple Navbar (grid view) and remove complex landing navbar

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,5 +1,5 @@
-import { Navbar } from '@/components/landing/Navbar'
-import { Footer } from '@/components/landing/Footer'
+import { Navbar } from '@/components/layout/Navbar'
+import { Footer } from '@/components/layout/Footer'
 
 export default function MarketingLayout({
   children,
